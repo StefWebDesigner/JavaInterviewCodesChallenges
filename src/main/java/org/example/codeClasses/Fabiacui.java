c5913ffcaddfd386ca2a976474a5202d912f8801
Stefan Masanet Riley <Stefan@Stefans-MacBook-Pro.local>
2024-07-21 03:05:07 -0400

    adde improved fibiacci

--- src/main/java/org/example/codeClasses/Fabiacui.java
+++ src/main/java/org/example/codeClasses/Fabiacui.java
@@ -15,4 +15,25 @@ public class Fabiacui {
 
     }
 
+    public int fibSimpleAndImproved(int n){
+        int num1 = 0, num2 = 1;
+
+        int counter = 0;
+
+        // Iterate till counter is N
+        while (counter < n) {
+
+            // Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
+        return num1;
+    }
+
+
 }
