10c206a4376463e02a148fe736b5479276d58e69
Stefan <stefan_riley@icloud.com>
2025-04-01 14:56:34 -0400

    changes stuff

--- src/main/java/org/example/classbox/LongestCommonPrefix.java
+++ src/main/java/org/example/classbox/LongestCommonPrefix.java
@@ -6,15 +6,23 @@ public class LongestCommonPrefix {
 
     public String longestCommonPrefix() {
         String[] strs = {"flower", "flow", "flight"};
+        String result = "";
+
         int numberOfStrings = strs.length;
-        for (int index = 0; index < strs[0].length(); ++index) {
-            for (int stringIndex = 1; stringIndex < numberOfStrings; ++stringIndex) {
-                if (strs[stringIndex].length() <= index || strs[stringIndex].charAt(index) != strs[0].charAt(index)) {
-                    return strs[0].substring(0, index);
+        String firstWord = strs[0];
+        //Handles the firstword
+        //Index i handles collecting the string till it break
+        for (int i = 0; i < firstWord.length(); ++i) {
+            //This handles the entire String array
+            for (int s = 1; s < numberOfStrings; ++s) {
+                //Detects if the sequence has been broken, ties up the string, and returns
+                if (strs[s].length() <= i || strs[s].charAt(i) != firstWord.charAt(i)) {
+                    return firstWord.substring(0, i);
                 }
             }
         }
-        return strs[0];
+        return firstWord;
+
     }
 
 
