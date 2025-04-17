f68b8dadc80cccc8368437a013a9631c8b16fe0b
Stefan <stefan_riley@icloud.com>
2025-04-16 22:25:20 -0400

    revised it

--- src/main/java/org/example/classbox/medium/FindSumTriplet.java
+++ src/main/java/org/example/classbox/medium/FindSumTriplet.java
@@ -11,24 +11,32 @@ public class FindSumTriplet {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+            if(i >  0 && nums[i] == nums[i -1]){
                 continue;
             }
-            int left = i + 1;
-            int right = nums.length - 1;
 
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == target) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (sum < target) {
-                    left++;
+            int l = i + 1;
+            int r = nums.length -1;
+
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == target){
+                    //Achieved the answer -- but waiting to see if more results come up
+                    result.add(Arrays.asList(nums[i], nums[l], nums[r]));
+
+                    while( l < r && nums[l] == nums[l + 1]){
+                        l++;
+                    }
+                    while(l < r && nums[r] == nums[r -1]){
+                        r--;
+                    }
+
+                    l++;
+                    r--;
+                } else if (sum < target){
+                    l++;
                 } else {
-                    right--;
+                    r--;
                 }
             }
         }
