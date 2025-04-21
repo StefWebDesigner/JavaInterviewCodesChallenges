f596f7b172845ca6e2d031e5ca487914cdf38e10
Stefan <stefan_riley@icloud.com>
2025-04-21 13:04:15 -0400

    added ThreeSumClosest

---
+++ src/main/java/org/example/classbox/medium/ThreeSumClosest.java
@@ -0,0 +1,32 @@
+package org.example.classbox.medium;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+
+//    Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is
+//    closest to target.
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int ans = 1 << 30;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int t = nums[i] + nums[j] + nums[k];
+                if (t == target) {
+                    return t;
+                }
+                if (Math.abs(t - target) < Math.abs(ans - target)) {
+                    ans = t;
+                }
+                if (t > target) {
+                    --k;
+                } else {
+                    ++j;
+                }
+            }
+        }
+        return ans;
+    }
+}
