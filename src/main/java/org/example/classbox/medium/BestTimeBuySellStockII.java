2f2115eb5bf264c4eef8940250341bbc55970433
Stefan <stefan_riley@icloud.com>
2025-04-30 21:48:18 -0400

    asdasd

--- src/main/java/org/example/classbox/medium/BestTimeBuySellStockII.java
+++ src/main/java/org/example/classbox/medium/BestTimeBuySellStockII.java
@@ -3,7 +3,9 @@ package org.example.classbox.medium;
 public class BestTimeBuySellStockII {
     public int maxProfit(int[] prices) {
         int totalProfit = 0;
+        //Go into the loop
         for (int i = 1; i < prices.length; ++i) {
+            //Find the price is bigger or smaller
             int dailyProfit = Math.max(0, prices[i] - prices[i - 1]);
             totalProfit += dailyProfit;
         }
